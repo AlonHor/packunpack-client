4c1292ab3227e631b1944a95039761071be69dd1
Alon <alon.horesh.08@gmail.com>
2022-09-14 12:35:18 +0300

    Make errors more specific

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,25 +1,25 @@
 function redirectToDashboard() {
-  localStorage.setItem('sid', '1149a53e-9e8c-4d96-bd7b-8b3d5bb0d67a'); // hardcoded sid for google adsense.
-  window.location.href = '/dashboard';
+  localStorage.setItem("sid", "1149a53e-9e8c-4d96-bd7b-8b3d5bb0d67a"); // hardcoded sid for google adsense.
+  window.location.href = "/dashboard";
 }
 
-localStorage.removeItem('sid');
-const loginForm = document.getElementById('login-form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-password.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
+localStorage.removeItem("sid");
+const loginForm = document.getElementById("login-form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+password.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
     e.preventDefault();
   }
 });
-loginForm.addEventListener('submit', (e) => {
+loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const usernameValue = username.value;
   const passwordValue = password.value;
   fetch(`${server}/login`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       username: usernameValue,
@@ -30,26 +30,29 @@ loginForm.addEventListener('submit', (e) => {
       if (res.status === 200) {
         return res.json();
       } else {
-        throw new Error('Failed to login');
+        throw new Error(res.json().message);
+        // throw new Error("Failed to login");
       }
     })
     .then((res) => {
       if (res.sid) {
         if (res.id) {
-          localStorage.setItem('sid', res.sid);
-          window.location.href = '/dashboard';
+          localStorage.setItem("sid", res.sid);
+          window.location.href = "/dashboard";
         } else {
           if (res.message) {
             alert(res.message);
           } else {
-            throw new Error('Failed to login');
+            throw new Error(res.json().message);
+            // throw new Error("Failed to login");
           }
         }
       } else {
         if (res.message) {
           alert(res.message);
         } else {
-          throw new Error('Failed to login');
+          throw new Error(res.json().message);
+          // throw new Error("Failed to login");
         }
       }
     })
--- assets/js/register.js
+++ assets/js/register.js
@@ -1,24 +1,24 @@
-localStorage.removeItem('sid');
-const registerForm = document.getElementById('register-form');
-const email = document.getElementById('email');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const type = document.getElementById('type');
-password.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
+localStorage.removeItem("sid");
+const registerForm = document.getElementById("register-form");
+const email = document.getElementById("email");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const type = document.getElementById("type");
+password.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
     e.preventDefault();
   }
 });
-registerForm.addEventListener('submit', (e) => {
+registerForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const emailValue = email.value;
   const usernameValue = username.value;
   const passwordValue = password.value;
   const typeValue = type.value;
   fetch(`${server}/register`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       email: emailValue,
@@ -31,26 +31,29 @@ registerForm.addEventListener('submit', (e) => {
       if (res.status === 200) {
         return res.json();
       } else {
-        throw new Error('Failed to login');
+        throw new Error(res.json().message);
+        // throw new Error('Failed to register');
       }
     })
     .then((res) => {
       if (res.sid) {
         if (res.id) {
-          localStorage.setItem('sid', res.sid);
-          window.location.href = '/dashboard';
+          localStorage.setItem("sid", res.sid);
+          window.location.href = "/dashboard";
         } else {
           if (res.message) {
             alert(res.message);
           } else {
-            throw new Error('Failed to login');
+            throw new Error(res.json().message);
+            // throw new Error('Failed to register');
           }
         }
       } else {
         if (res.message) {
           alert(res.message);
         } else {
-          throw new Error('Failed to login');
+          throw new Error(res.json().message);
+          // throw new Error('Failed to register');
         }
       }
     })
