28c2e1ad70c7fd88b63fa04318ac0ff7210237db
Alon <alon.horesh.08@gmail.com>
2022-09-15 18:31:08 +0300

    Fix cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,12 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticCache = "dev-coffee-site-v1";
 const assets = ["/assets/img/favicon.ico"];
 
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then((cache) => {
+    caches.open(staticCache).then((cache) => {
+      caches.keys().then((names) => {
+        for (let name of names) caches.delete(name);
+      });
       cache.addAll(assets);
     })
   );
