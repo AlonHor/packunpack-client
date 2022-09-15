64df3dcfdbb5780ebf2a392304d17d2f3a48c989
Alon <alon.horesh.08@gmail.com>
2022-09-15 15:55:51 +0300

    Fix tiny mistake in animation.js

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -2,10 +2,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("visible");
-      entry.target.classList.remove("hidden");
     } else {
       entry.target.classList.remove("visible");
-      entry.target.classList.add("hidden");
     }
   });
 });
