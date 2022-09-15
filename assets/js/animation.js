7c855129848356d8c8e901512b22b087aa5c6453
Alon <alon.horesh.08@gmail.com>
2022-09-15 15:30:06 +0300

    Add cool animation for the divs

---
+++ assets/js/animation.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
