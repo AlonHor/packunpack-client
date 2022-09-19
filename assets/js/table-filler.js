7d57136ae2a4195379f83c7927cdef9aa775768d
Alon <alon.horesh.08@gmail.com>
2022-09-19 14:38:43 +0300

    Switched back to event listener

--- assets/js/table-filler.js
+++ assets/js/table-filler.js
@@ -1,5 +1,5 @@
 let audio = new Audio("/assets/sounds/rickroll.mp3");
-on("visibilitychange", () => {
+window.addEventListener("visibilitychange", () => {
   console.log(document.visibilityState);
   if (document.visibilityState === "visible") {
     audio.pause();
