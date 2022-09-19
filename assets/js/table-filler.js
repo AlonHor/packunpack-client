055f0eef1e056eacc294355948596f9905c7456d
Alon <alon.horesh.08@gmail.com>
2022-09-19 20:47:35 +0300

    Remove rickroll

--- assets/js/table-filler.js
+++ assets/js/table-filler.js
@@ -1,14 +1,3 @@
-let audio = new Audio("/assets/sounds/rickroll.mp3");
-window.addEventListener("visibilitychange", () => {
-  console.log(document.visibilityState);
-  if (document.visibilityState === "visible") {
-    audio.pause();
-  } else {
-    audio.currentTime = 0;
-    audio.play();
-  }
-});
-
 let theNamesOfTheDaysOfTheWeek = [
   "monday",
   "tuesday",
